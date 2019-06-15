d75e2b7d619e59278ecc5e745766674ac205a52b
Xanedithas <mikestaadegaard@hotmail.com>
2019-06-15 18:06:58 +0200

    Basic search input with backend communication

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 const app = express();
+app.use(express.static('./src/public'))
 
 import PdfParser from './Modules/PdfParser';
-
 const pdfparser = new PdfParser();
 
 const hostname = '127.0.0.1';
@@ -24,6 +24,27 @@ app.get('/about', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.send('About page');
+});
+
+app.get('/search', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'html');
+  res.sendFile('public/search.html' , { root : __dirname});
+});
+
+app.get('/search/:query', (req, res) => {
+  if (req.params) {
+    console.log('req.params!');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    //req.body.query[0] = capitalize(req.body.query[0]);
+    console.log(req.params);
+    res.send(req.params);
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'html');
+    res.sendFile('public/search.html' , { root : __dirname});
+  }
 });
 
 app.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}/`));
