8ccd11a3e31a466260f2cb71cce70c0eb97a1585
Steven Gosseling <stevengosseling@hotmail.com>
2019-06-15 12:20:33 +0200

    implemented folder parser

--- Modules/PdfParser.js
+++ Modules/PdfParser.js
@@ -1,9 +1,45 @@
 const fs = require('fs');
+const fsp = require('fs').promises;
+const path = require('path');
 const pdf = require('pdf-parse');
 
 class PdfParser{
     constructor(){
-        
+    }
+
+    static async parsePDFFolder(){
+        let directoryPath = path.join(__dirname,'../Files');
+        let arr = [];
+
+        arr = await fsp.readdir(directoryPath, function (err, files) {
+            let fileArr = [];
+
+            //handling error
+            if (err) {
+                return console.log('Unable to scan directory: ' + err);
+            }
+            //listing all files using forEach
+            files.forEach(function (file) {
+                // Do whatever you want to do with the file
+                fileArr.push(file);
+            });
+
+            return fileArr;
+        });
+
+        return PdfParser.parsePDFArr(arr);
+    }
+
+    static async parsePDFArr(arr, index = 0, resultArr = []){
+        if(index >= arr.length){
+            return resultArr;
+        }
+
+        let elem = arr[index];
+        let parsedResult = await PdfParser.parsePdf('./Files'+'/'+elem);
+
+        resultArr.push(parsedResult);
+        return PdfParser.parsePDFArr(arr, ++index, resultArr);
     }
 
     static parsePdf(path){
@@ -20,9 +56,7 @@ class PdfParser{
     static textToJSON(data){
         let parsedData = data.split(/(?:\n){2,}/g);
 
-        console.log(parsedData);
-
-        return JSON.stringify(parsedData);
+        return parsedData;
     }
 }
 module.exports = PdfParser;
--- app.js
+++ app.js
@@ -6,27 +6,30 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  let tempResult = PdfParser.parsePdf('./test.pdf');
-
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  tempResult.then((PDFres) => {
-    // console.log(PDFres.text);
-
-    fs.writeFile("./test.txt", PDFres, function(err) {
-      if(err) {
-        return console.log(err);
-      }
-
-      console.log("The file was saved!");
-    });
-
-    res.end(PDFres);
+// const server = http.createServer((req, res) => {
+  PdfParser.parsePDFFolder().then((res) => {
+    console.log(res);
   });
-});
+  // let tempResult = PdfParser.parsePdf('./test.pdf');
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+
+  // tempResult.then((PDFres) => {
+  //   // console.log(PDFres.text);
+  //
+  //   fs.writeFile("./test.txt", PDFres, function(err) {
+  //     if(err) {
+  //       return console.log(err);
+  //     }
+  //
+  //     console.log("The file was saved!");
+  //   });
+  //
+  //   // res.end(PDFres);
+  // });
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
